e9ba7bb82aa8beeb912b22372c2ed959fcb60f44
AkimotoHaruma <siba16fb@gmail.com>
2022-05-07 14:16:24 +0900

    add reception main.py

---
+++ reception_main/script/reception_main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import Bool
+
+from time import sleep
+
+import simple_push_button.srv
+
+rospy.init_node('reception_main',anonymous=True)
+
+led_blink = rospy.ServiceProxy( 'buttun_led_blink', simple_push_button.srv.Buttionblink )
+
+def cb_human_detect(data):
+    led_blink()
+    
+
+rospy.Subscliber( 'human_detect', cb_human_detect)
+
+if __name__ == '__main__':
+
+    rate = rospy.Rate(50)
+
+    try:
+        while not rospy.is_shutdown():
+            #for now 
+            rate.sleep()
+
+    except KeyboardInterrupt:
+        #pi.write(PIN_SWITCH_LED,pigpio.LOW)
+        pass
+    finally:
+        #pi.write(PIN_SWITCH_LED,pigpio.LOW)
+        pass
