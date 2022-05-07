3ac370d760dfb8401884dfba80f641c7ace5e27c
AkimotoHaruma <siba16fb@gmail.com>
2022-05-07 06:36:27 +0000

    fix bug

--- reception_main/script/reception_main.py
+++ reception_main/script/reception_main.py
@@ -6,18 +6,19 @@ from std_msgs.msg import Bool
 
 from time import sleep
 
-import simple_push_button.srv
+from simple_push_button.srv import Buttonblink
 
 rospy.init_node('reception_main',anonymous=True)
 
-led_blink = rospy.ServiceProxy( 'buttun_led_blink', simple_push_button.srv.Buttionblink )
+led_blink = rospy.ServiceProxy( 'buttun_led_blink', Buttonblink )
 
-def cb_human_detect(data):
+def cb_human_detect(human_sensor):
     rospy.loginfo("main cb_human_detect")
-    led_blink()
+    if human_sensor.data == True:
+        led_blink()
 
 
-rospy.Subscliber( 'human_detect', cb_human_detect)
+rospy.Subscriber( 'human_detect', Bool,cb_human_detect)
 
 if __name__ == '__main__':
 
